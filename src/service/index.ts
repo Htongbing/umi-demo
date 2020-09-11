6b13304638d7512291d82f7cf020dad060cb29b3
Htongbing <1521855115@qq.com>
2020-09-11 16:27:51 +0800

    update

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,10 +1,16 @@
-import axios, { AxiosInstance } from 'axios'
-import { AXIOS_BASE_URL } from '@/const'
+import axios, { AxiosInstance } from 'axios';
+import { AXIOS_BASE_URL } from '@/const';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: AXIOS_BASE_URL
-})
+  baseURL: AXIOS_BASE_URL,
+});
 
-axiosInstance.interceptors.response.use(res => Promise.resolve(res.data))
+axiosInstance.interceptors.response.use(res => {
+  const { data } = res;
+  if (data.rescode === '0') {
+    return Promise.resolve(data);
+  }
+  return Promise.reject(data);
+});
 
-export default axiosInstance
+export default axiosInstance;
